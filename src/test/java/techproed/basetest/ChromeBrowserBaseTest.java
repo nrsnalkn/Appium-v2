14315acb28c52e00809d4ef8229bbdb0b3b07f62
nrsn <nursenalkan84@gmail.com>
2023-12-09 20:33:08 +0300

    v2

--- src/test/java/techproed/basetest/ChromeBrowserBaseTest.java
+++ src/test/java/techproed/basetest/ChromeBrowserBaseTest.java
@@ -1,35 +1,26 @@
 package techproed.basetest;
-
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import techproed.utilities.ReusableMethods;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
-public class DeviceBaseTest extends ReusableMethods {
-    //boş testbase herhangi bir app açmaz cihaz acar cihaz testi için kullanılır
-    // driver kullanmak için oluşturuldu
+public class ChromeBrowserBaseTest extends ReusableMethods {
     protected AndroidDriver driver;
-
     @BeforeClass
     public void setup() throws MalformedURLException {
-
         UiAutomator2Options options = new UiAutomator2Options()
-//                .setUnlockType("pattern")//pin seceneğide kullanılabilir
-//                .setUnlockKey("1236")
+                .withBrowserName("chrome")
                 ;
 
         URL url = new URL("http://0.0.0.0:4723");
-
         driver = new AndroidDriver(url, options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
-
     @AfterClass
     public void afterClass() {
         driver.quit();
